e066837f92a2296b7f75dfdd6122481b253af6e3
HyeonJun <hyeonjun1968@naver.com>
2025-05-13 18:33:05 +0900

    chore : project setting

---
+++ src/main/java/com/cmu02/airmeet_be/AirMeetBeApplication.java
@@ -0,0 +1,13 @@
+package com.cmu02.airmeet_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirMeetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirMeetBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmu02/airmeet_be/AirMeetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmu02.airmeet_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirMeetBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
