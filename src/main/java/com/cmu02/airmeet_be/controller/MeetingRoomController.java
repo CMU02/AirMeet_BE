4a331f5500f587b99ac467e2bef1526fcda10d05
HyeonJun <hyeonjun1968@naver.com>
2025-05-14 21:59:09 +0900

    feat: 회의방 생성 엔드포인트 및 MeetingRoomService 연결 컨트롤러 구현

--- src/main/java/com/cmu02/airmeet_be/controller/MeetingRoomController.java
+++ src/main/java/com/cmu02/airmeet_be/controller/MeetingRoomController.java
@@ -2,18 +2,21 @@ package com.cmu02.airmeet_be.controller;
 
 import com.cmu02.airmeet_be.domain.dto.request.MeetingRoomRequest;
 import com.cmu02.airmeet_be.domain.dto.response.MeetingRoomResponse;
-import com.cmu02.airmeet_be.services.MeetingRoomRedisService;
+import com.cmu02.airmeet_be.services.MeetingRoomService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/rooms")
 @RequiredArgsConstructor
 public class MeetingRoomController {
-    private final MeetingRoomRedisService service;
+    private final MeetingRoomService service;
 
     @PostMapping
     public Mono<ResponseEntity<MeetingRoomResponse>> createRoom(@RequestBody @Valid MeetingRoomRequest request) {
