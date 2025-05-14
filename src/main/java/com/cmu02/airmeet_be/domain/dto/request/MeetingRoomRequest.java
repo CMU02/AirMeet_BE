f93099f54679c09b67850339da0f61c412a5ed9d
HyeonJun <hyeonjun1968@naver.com>
2025-05-14 21:59:23 +0900

    feat: 회의방 생성 요청용 MeetingRoomRequest 레코드 클래스 추가

--- src/main/java/com/cmu02/airmeet_be/domain/dto/request/MeetingRoomRequest.java
+++ src/main/java/com/cmu02/airmeet_be/domain/dto/request/MeetingRoomRequest.java
@@ -1,9 +1,10 @@
 package com.cmu02.airmeet_be.domain.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
 public record MeetingRoomRequest(
-        @NotBlank String host,
-        @NotBlank String roomName
+        @NotBlank String roomName,
+        @Valid  UserRequestDto user
 ) {
 }
