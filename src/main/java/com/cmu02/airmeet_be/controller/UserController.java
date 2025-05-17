63fba33fcd3b9babd56ddfdbce6db2b84af914dc
HyeonJun <hyeonjun1968@naver.com>
2025-05-17 18:24:45 +0900

    feat : 해당 유저가 들어가있는 모든 회의방 조회 기능 추가
    - KeySuffix 추가 및 서비스 로직 Refactoring

--- src/main/java/com/cmu02/airmeet_be/controller/UserController.java
+++ src/main/java/com/cmu02/airmeet_be/controller/UserController.java
@@ -2,16 +2,15 @@ package com.cmu02.airmeet_be.controller;
 
 import com.cmu02.airmeet_be.domain.dto.request.AddUserRequestDto;
 import com.cmu02.airmeet_be.domain.dto.request.JoinRoomRequestDto;
+import com.cmu02.airmeet_be.domain.dto.request.UserRequestDto;
 import com.cmu02.airmeet_be.domain.dto.response.MeetingRoomResponse;
 import com.cmu02.airmeet_be.domain.dto.response.UserResponseDto;
 import com.cmu02.airmeet_be.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,15 +19,23 @@ import reactor.core.publisher.Mono;
 public class UserController {
     private final UserService service;
 
+    // 유저 생성
     @PostMapping("/add")
     public Mono<ResponseEntity<UserResponseDto>> addUser(@RequestBody AddUserRequestDto dto) {
         return service.addUser(dto)
                 .map(ResponseEntity::ok);
     }
 
+    // 조인코드를 이용하여 회의방 참가
     @PostMapping("/joinRoom")
     public Mono<ResponseEntity<MeetingRoomResponse>> joinRoom(@RequestBody @Valid JoinRoomRequestDto dto) {
         return service.joinRoomByCode(dto)
                 .map(ResponseEntity::ok);
+    }
+
+    // 해당 유저가 참가하고 있는 방 조회
+    @GetMapping("/rooms")
+    public Flux<MeetingRoomResponse> getRoomsByUser(@RequestBody UserRequestDto dto) {
+        return service.getRoomsByUser(dto);
     }
 }
