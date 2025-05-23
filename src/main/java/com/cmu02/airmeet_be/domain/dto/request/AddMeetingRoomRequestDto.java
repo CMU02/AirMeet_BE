4aa0ddebee8d19eceea4319589df21c9d3169a70
HyeonJun <hyeonjun1968@naver.com>
2025-05-23 17:17:05 +0900

    refactor : 파일이름 naming Convention에 맞게 수정

--- src/main/java/com/cmu02/airmeet_be/domain/dto/request/AddMeetingRoomRequestDto.java
+++ src/main/java/com/cmu02/airmeet_be/domain/dto/request/AddMeetingRoomRequestDto.java
@@ -3,7 +3,7 @@ package com.cmu02.airmeet_be.domain.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
-public record MeetingRoomRequest(
+public record AddMeetingRoomRequestDto(
         @NotBlank String roomName,
         @Valid  UserRequestDto user
 ) {
