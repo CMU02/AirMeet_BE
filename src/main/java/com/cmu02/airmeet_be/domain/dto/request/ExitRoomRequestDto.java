f2073db20da530d075dc45b528bea30cfaa4f27b
HyeonJun <hyeonjun1968@naver.com>
2025-05-23 17:17:05 +0900

    refactor : 파일이름 naming Convention에 맞게 수정

--- src/main/java/com/cmu02/airmeet_be/domain/dto/request/ExitRoomRequestDto.java
+++ src/main/java/com/cmu02/airmeet_be/domain/dto/request/ExitRoomRequestDto.java
@@ -2,7 +2,7 @@ package com.cmu02.airmeet_be.domain.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record ExitRoomReqDto(
+public record ExitRoomRequestDto(
         @NotBlank String uuid,
         @NotBlank String roomId
 ) {
